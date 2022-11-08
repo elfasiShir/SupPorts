ccaaf6fb3aab387ed7507ebd16ca186b767b4517
shir <torushojiro@gmail.com>
2022-11-08 11:28:19 +0200

    first commit

---
+++ SupPorts.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SupPorts_V2
+{
+    partial class SupPorts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FindPorts = new System.Windows.Forms.Button();
+            this.IpInput = new System.Windows.Forms.TextBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.IpInputHeader = new System.Windows.Forms.Label();
+            this.IpThreadManager = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // FindPorts
+            // 
+            this.FindPorts.Location = new System.Drawing.Point(0, 0);
+            this.FindPorts.Name = "FindPorts";
+            this.FindPorts.Size = new System.Drawing.Size(131, 39);
+            this.FindPorts.TabIndex = 0;
+            this.FindPorts.Text = "FindPorts";
+            this.FindPorts.UseVisualStyleBackColor = true;
+            this.FindPorts.Click += new System.EventHandler(this.FindPorts_Click);
+            // 
+            // IpInput
+            // 
+            this.IpInput.Location = new System.Drawing.Point(0, 96);
+            this.IpInput.Name = "IpInput";
+            this.IpInput.Size = new System.Drawing.Size(131, 20);
+            this.IpInput.TabIndex = 1;
+            this.IpInput.TextChanged += new System.EventHandler(this.IpInput_TextChanged);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(361, 26);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(62, 13);
+            this.labelOutput.TabIndex = 2;
+            this.labelOutput.Text = "label output";
+            // 
+            // IpInputHeader
+            // 
+            this.IpInputHeader.AutoSize = true;
+            this.IpInputHeader.Location = new System.Drawing.Point(-3, 80);
+            this.IpInputHeader.Name = "IpInputHeader";
+            this.IpInputHeader.Size = new System.Drawing.Size(46, 13);
+            this.IpInputHeader.TabIndex = 3;
+            this.IpInputHeader.Text = "Enter ip:";
+            this.IpInputHeader.Click += new System.EventHandler(this.IpInputHeader_Click);
+            // 
+            // IpThreadManager
+            // 
+            this.IpThreadManager.DoWork += new System.ComponentModel.DoWorkEventHandler(this.IpThreadManager_DoWork);
+            // 
+            // SupPorts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.IpInputHeader);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.IpInput);
+            this.Controls.Add(this.FindPorts);
+            this.Name = "SupPorts";
+            this.Text = "SupPorts";
+            this.Load += new System.EventHandler(this.SupPorts_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FindPorts;
+        private System.Windows.Forms.TextBox IpInput;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Label IpInputHeader;
+        private System.ComponentModel.BackgroundWorker IpThreadManager;
+    }
+}
+
---
+++ SupPorts.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SupPorts_V2
+{
+    public partial class SupPorts : Form
+    {
+        //initialize ip with local host
+        public IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+        public SupPorts()
+        {
+            InitializeComponent();
+            labelOutput.Text += "\n";
+        }
+
+        private void IpInput_TextChanged(object sender, EventArgs e)
+        {
+            if (IPAddress.TryParse(IpInput.Text, out this.ip)) {
+                IpInputHeader.Text = "Enter ip: valid " + this.ip.ToString();
+            } else {
+                IpInputHeader.Text = "Enter ip: not an ip";
+            }
+        }
+        private void FindPorts_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text += "Analyzing ip: " + this.ip.ToString() + "\n";
+
+        }
+
+        private void SupPorts_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IpInputHeader_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IpThreadManager_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+    }
+}
